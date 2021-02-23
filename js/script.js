e7b96c7002ad41557d0c385df0b73d6e71e0bd65
devb0x <moniezgeoffrey59@gmail.com>
2021-02-23 02:35:24 +0100

    project end

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-// 10K pageviews / \$8 per month
-// 50K pageviews / \$12 per month
-// 100K pageviews / \$16 per month
-// 500k pageviews / \$24 per month
-// 1M pageviews / \$36 per month
-
 const number_span = document.querySelector('.userChoice');
 const slider_input = document.getElementById('viewNumber');
 const price_div = document.querySelector('.price');
 
-// const billing_div = document.querySelector('.switch');
-const billing_div = document.getElementById('test');
+const billing_div = document.querySelector('input[type=checkbox]');
 
 const pageViews = [
   '10K', '50K', '100K', '500K', '1M'
@@ -19,39 +12,31 @@ const prices = [
   8.00, 12.00, 16.00, 24.00, 36.00
 ];
 
-let billingDiscount;
-
-slider_input.addEventListener('change', () => {
-  let discountPrice = prices[slider_input.value] - prices[slider_input.value] * (25/100);
-  number_span.innerHTML = pageViews[slider_input.value];
-  price_div.innerHTML = '$' + `${discountPrice.toLocaleString('en-Gb',({minimumFractionDigits: 2}))}`;
-  renderPrice();
-})
-
-billing_div.addEventListener('change', (e) => {
-  let billingDiscount = event.target.checked;
-
-  if (event.target.checked) {
-    return billingDiscount === true;
-  }
-
-  console.log('event change');
-  return billingDiscount === false;
-  // billingDiscount = !billingDiscount;
-
-  if (event.target.checked) {
-    return billingDiscount === true
-  } else {
-    return billingDiscount === false
-  }
-});
+let billingDiscount = true;
+let discountPrice = prices[slider_input.value] - prices[slider_input.value] * (25/100);
 
 number_span.innerHTML = pageViews[slider_input.value];
 price_div.innerHTML = '$' + `${discountPrice.toLocaleString('en-Gb',({minimumFractionDigits: 2}))}`;
 renderPrice();
 
+slider_input.addEventListener('change', () => {
+  renderPrice();
+})
+
+billing_div.addEventListener('click', (e) => {
+  billingDiscount = billingDiscount === false;
+  renderPrice();
+});
 
 function renderPrice () {
+  let discountPrice = prices[slider_input.value] - prices[slider_input.value] * (25/100);
+  number_span.innerHTML = pageViews[slider_input.value];
+  if (billingDiscount === true) {
+    price_div.innerHTML = '$' + `${discountPrice.toLocaleString('en-Gb',({minimumFractionDigits: 2}))}`;
+  } else {
+    price_div.innerHTML = '$' + prices[slider_input.value].toLocaleString('en-Gb',({minimumFractionDigits: 2}));
+  }
+
   const span = document.createElement('span');
   span.innerHTML = ' / month';
   price_div.appendChild(span);
